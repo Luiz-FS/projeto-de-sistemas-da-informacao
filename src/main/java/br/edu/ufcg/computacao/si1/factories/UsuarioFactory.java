0d46ca153ef2adfd9325d57053b1a5be80715a2a
lucasvieira10 <lucas.vieira@ccc.ufcg.edu.br>
2017-03-08 19:26:09 -0300

    criacao do esqueleto da factory para Usuario

---
+++ src/main/java/br/edu/ufcg/computacao/si1/factories/UsuarioFactory.java
@@ -0,0 +1,27 @@
+package br.edu.ufcg.computacao.si1.factories;
+
+import br.edu.ufcg.computacao.si1.model.usuario.Usuario;
+
+/**
+ * Created by lucas on 08/03/17.
+ */
+
+public class UsuarioFactory {
+
+
+    public static Usuario criaPessoaFisica(String nome, String email, String senha) {
+
+
+        return null;
+
+    }
+
+    public static Usuario criaPessoaJuridica(String nome, String email, String senha) {
+
+
+        return null;
+
+    }
+
+
+}

3d583ba06213527300e4601a5d827fd8e892e4ec
lucasvieira10 <lucas.vieira@ccc.ufcg.edu.br>
2017-03-08 00:19:09 -0300

    colocando data de agendamento em AnuncioServico

--- src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/AnuncioServico.java
+++ src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/AnuncioServico.java
@@ -1,7 +1,30 @@
 package br.edu.ufcg.computacao.si1.model.anuncio;
 
+import br.edu.ufcg.computacao.si1.model.Notas;
+
+import javax.persistence.Column;
+import java.util.Date;
+
 /**
  * Created by luiz on 07/03/17.
  */
 public class AnuncioServico extends Anuncio {
+
+    @Column(name = "data_agendamento", nullable = false)
+    private Date dataDeAgendamento;
+
+    public AnuncioServico(String titulo, Date dataDeCriacao, Date dataDeAgendamento, double valor,
+                          Notas nota, String tipo) {
+        super(titulo,dataDeCriacao,valor,nota, tipo);
+
+        this.dataDeAgendamento = dataDeAgendamento;
+    }
+
+    public Date getDataDeAgendamento() {
+        return dataDeAgendamento;
+    }
+
+    public void setDataDeAgendamento(Date dataDeAgendamento) {
+        this.dataDeAgendamento = dataDeAgendamento;
+    }
 }

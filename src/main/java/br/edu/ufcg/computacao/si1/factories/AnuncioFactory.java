c913c7a73d8e0800e0c96a68de963cca2b3141e6
antonioPedroNeto <antonio.pedro.neto@ccc.ufcg.edu.br>
2017-03-08 08:27:07 -0300

    Adicionado argumentos de acordo com o construtor de AnuncioServico

--- src/main/java/br/edu/ufcg/computacao/si1/factories/AnuncioFactory.java
+++ src/main/java/br/edu/ufcg/computacao/si1/factories/AnuncioFactory.java
@@ -1,5 +1,6 @@
 package br.edu.ufcg.computacao.si1.factories;
 
+import br.edu.ufcg.computacao.si1.model.Notas;
 import br.edu.ufcg.computacao.si1.model.anuncio.Anuncio;
 import br.edu.ufcg.computacao.si1.model.anuncio.AnuncioEmprego;
 import br.edu.ufcg.computacao.si1.model.anuncio.AnuncioProduto;
@@ -13,23 +14,24 @@ import java.util.Date;
 public class AnuncioFactory {
 
 
-    public static Anuncio criaEmprego(Long idUsuario, String titulo, Date dataCriacao, double valor, String nota){
+    public static Anuncio criaEmprego(Long idUsuario, String titulo, Date dataCriacao, double valor, Notas nota){
 
         return new AnuncioEmprego();
 
     }
 
 
-    public static Anuncio criaProduto(Long idUsuario, String titulo, Date dataCriacao, double valor, String nota){
+    public static Anuncio criaProduto(Long idUsuario, String titulo, Date dataDeCriacao, double valor, Notas nota){
 
         return new AnuncioProduto();
 
     }
 
 
-    public static Anuncio criaServico(Long idUsuario, String titulo, Date dataCriacao, double valor, String nota){
+    public static Anuncio criaServico(Long idUsuario, String titulo, Date dataDeCriacao, Date dataDeAgendamento, double valor,
+                                      Notas nota, String tipo){
 
-        return new AnuncioServico();
+        return new AnuncioServico(titulo, dataDeCriacao, dataDeAgendamento, valor, nota, tipo);
 
     }
 

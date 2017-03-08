6b34132d1ccd3ff09831bfb85e77057ebeea0eef
lucasvieira10 <lucas.vieira@ccc.ufcg.edu.br>
2017-03-08 18:37:25 -0300

    Removendo atributo dataDeAgendamento do contrutor da classe AnuncioServico

--- src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/AnuncioServico.java
+++ src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/AnuncioServico.java
@@ -5,23 +5,13 @@ import br.edu.ufcg.computacao.si1.model.Notas;
 import javax.persistence.Column;
 import java.util.Date;
 
-/**
- * Created by luiz on 07/03/17.
- */
 public class AnuncioServico extends Anuncio {
 
-    @Column(name = "data_agendamento", nullable = false)
+    @Column(name = "data_agendamento")
     private Date dataDeAgendamento;
 
-    /**
-     * O atributo data de criação está ferindo a construção da classe
-     * Já que o auncio de serviço quando é criado ainda não possui um agendamento.
-     */
-    public AnuncioServico(String titulo, Date dataDeCriacao, Date dataDeAgendamento, double valor,
-                          Notas nota, String tipo) {
-        super(titulo,dataDeCriacao,valor,nota, tipo);
-
-        this.dataDeAgendamento = dataDeAgendamento;
+    public AnuncioServico(String titulo, Date dataDeCriacao, double valor, Notas nota, String tipo) {
+        super(titulo, dataDeCriacao, valor, nota, tipo);
     }
 
     public AnuncioServico() {

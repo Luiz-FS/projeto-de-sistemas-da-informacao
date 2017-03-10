05c3bd7206d201fb6825c4aec23ee60b7703f984
antonioPedroNeto <antonio.pedro.neto@ccc.ufcg.edu.br>
2017-03-10 14:43:56 -0300

    Adicionado @Entity, atributo dataDeAgendamento, metodo get e set de dataDeAgendamento

--- src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/AnuncioServico.java
+++ src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/AnuncioServico.java
@@ -3,9 +3,19 @@ package br.edu.ufcg.computacao.si1.model.anuncio;
 import br.edu.ufcg.computacao.si1.model.Notas;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Entity
 public class AnuncioServico extends Anuncio {
+
+    private final static DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");//isso sera removido pois temos uma classe CONSTANTES
+
+
+    @Column(name = "data_agendamento")
+    private Date dataDeAgendamento;
 
     public AnuncioServico(String titulo, Date dataDeCriacao, double valor, Notas nota) {
         super(titulo, dataDeCriacao, valor, nota);
@@ -15,13 +25,11 @@ public class AnuncioServico extends Anuncio {
         super();
     }
 
-    @Override
-    public Date getDataDeAgendamento() {
-        return super.dataDeAgendamento;
+    public String getDataDeAgendamento() {
+        return DATE_FORMAT.format(this.dataDeAgendamento);
     }
 
-    @Override
     public void setDataDeAgendamento(Date dataDeAgendamento) {
-        super.dataDeAgendamento = dataDeAgendamento;
+        this.dataDeAgendamento = dataDeAgendamento;
     }
 }

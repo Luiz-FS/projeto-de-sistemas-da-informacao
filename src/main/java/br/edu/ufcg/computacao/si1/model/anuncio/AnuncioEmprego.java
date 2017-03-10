cb434a31707cf191b84553e9d25bb07ee1af2b9d
antonioPedroNeto <antonio.pedro.neto@ccc.ufcg.edu.br>
2017-03-10 14:41:04 -0300

    Adicionado @Entity

--- src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/AnuncioEmprego.java
+++ src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/AnuncioEmprego.java
@@ -2,11 +2,13 @@ package br.edu.ufcg.computacao.si1.model.anuncio;
 
 import br.edu.ufcg.computacao.si1.model.Notas;
 
+import javax.persistence.Entity;
 import java.util.Date;
 
 /**
  * Created by luiz on 07/03/17.
  */
+@Entity
 public class AnuncioEmprego extends Anuncio {
 
     public AnuncioEmprego(String titulo, Date dataCriacao, Notas nota) {

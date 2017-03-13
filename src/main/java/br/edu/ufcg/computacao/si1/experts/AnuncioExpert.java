0db1b0dff0d377ecf60871a045a460ca9181f0bc
davilaerte <davi.nascimento@ccc.ufcg.edu.br>
2017-03-13 00:49:35 -0300

    adicionando metodo para salvar anuncio no BD

--- src/main/java/br/edu/ufcg/computacao/si1/experts/AnuncioExpert.java
+++ src/main/java/br/edu/ufcg/computacao/si1/experts/AnuncioExpert.java
@@ -1,15 +1,23 @@
 package br.edu.ufcg.computacao.si1.experts;
 
-import br.edu.ufcg.computacao.si1.model.Notas;
-import br.edu.ufcg.computacao.si1.util.Validador;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Date;
-
+import br.edu.ufcg.computacao.si1.model.anuncio.Anuncio;
+import br.edu.ufcg.computacao.si1.repository.AnuncioRepository;
 
 /**
  * Created by Antonio Pedro on 07/03/2017.
  */
 public class AnuncioExpert {
-
+	
+	@Autowired
+	private AnuncioRepository repositorioAnuncio;
+	
+	
+	public Anuncio salvarAnuncio(Anuncio anuncio) {
+		return repositorioAnuncio.save(anuncio);
+	}
+	
+	
 
 }

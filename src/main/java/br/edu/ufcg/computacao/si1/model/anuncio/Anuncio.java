1ee800975809128c1d37a34fc921fd1f739e438b
lucasvieira10 <lucas.vieira@ccc.ufcg.edu.br>
2017-03-13 21:13:00 -0300

    resolvendo problemas de conflito com a branch usuario

--- src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/Anuncio.java
+++ src/main/java/br/edu/ufcg/computacao/si1/model/anuncio/Anuncio.java
@@ -5,18 +5,11 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 @Entity
 @Table(name = "tb_anuncio")
 public abstract class Anuncio {
 
-	private final static DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+	protected final static DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -35,11 +28,6 @@ public abstract class Anuncio {
 	@Column(name = "valor")
 	private double valor;
 
-	@Column(name = "nota")
-	private AvaliacaoDeAnuncio nota;
-
-	public Anuncio(String titulo, Date dataDeCriacao, double valor, AvaliacaoDeAnuncio nota) {
-
 	@Column(name = "tipo")
 	private TipoAnuncio tipo;
 
@@ -47,7 +35,6 @@ public abstract class Anuncio {
 		this.titulo = titulo;
 		this.dataDeCriacao = dataDeCriacao;
 		this.valor = valor;
-		this.nota = nota;
 		this.tipo = tipo;
 		this.idUsuario = idUsuario;
 	}
@@ -57,14 +44,8 @@ public abstract class Anuncio {
 		this.dataDeCriacao = new Date();
 		this.valor = 0;
 		this.tipo = TipoAnuncio.DEFAULT;
-		this.nota = AvaliacaoDeAnuncio.NOTA_ZERO;
 	}
 
-	/**
-	 * Retorna o id do anuncio
-	 * 
-	 * @return o id do anuncio
-	 */
 	public Long getId() {
 		return _id;
 	}
@@ -91,61 +72,13 @@ public abstract class Anuncio {
 
 	public TipoAnuncio getTipo() {
 		return tipo;
-	public AvaliacaoDeAnuncio getNota() {
-		return nota;
 	}
 
 	public void setTipo(TipoAnuncio tipo) {
 		this.tipo = tipo;
-	public void setNota(AvaliacaoDeAnuncio nota) {
-		this.nota = nota;
 	}
 
 	public Long getIdUsuario() {
 		return idUsuario;
-	}
-
-
-	@Override
-	public boolean equals(Object object) {
-		if (this == object)
-			return true;
-		if (!(object instanceof Anuncio))
-			return false;
-
-		Anuncio anuncio = (Anuncio) object;
-
-		if (Double.compare(anuncio.getValor(), getValor()) != 0)
-			return false;
-		if (!_id.equals(anuncio._id))
-			return false;
-		if (!idUsuario.equals(anuncio.idUsuario))
-			return false;
-		if (!getTitulo().equals(anuncio.getTitulo()))
-			return false;
-		if (!getDataDeCriacao().equals(anuncio.getDataDeCriacao()))
-			return false;
-		return getNota() == anuncio.getNota();
-	}
-
-	@Override
-	public int hashCode() {
-		int result;
-		long temp;
-		result = _id.hashCode();
-		result = 31 * result + idUsuario.hashCode();
-		result = 31 * result + getTitulo().hashCode();
-		result = 31 * result + getDataDeCriacao().hashCode();
-		temp = Double.doubleToLongBits(getValor());
-		result = 31 * result + (int) (temp ^ (temp >>> 32));
-		result = 31 * result + getNota().hashCode();
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "Anuncio{" + "_id=" + _id + ", titulo='" + titulo + '\'' + ", dataDeCriacao=" + getDataDeCriacao()
-				+ ", valor=" + valor + ", nota=" + nota +
-				/* ", tipo='" + tipo + '\'' */ +'}';
 	}
 }

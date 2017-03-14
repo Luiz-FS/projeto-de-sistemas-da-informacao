394a02e12b5f71e2fa6f769dea01953ee1209a5e
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-03-13 23:25:16 -0300

    Modificando classe token para deixar métodos mais enchutos

--- src/main/java/br/edu/ufcg/computacao/si1/util/TokenCodificadorDecodificador.java
+++ src/main/java/br/edu/ufcg/computacao/si1/util/TokenCodificadorDecodificador.java
@@ -12,65 +12,55 @@ import java.util.*;
  */
 public class TokenCodificadorDecodificador {
 
-    private Set<Key> chavesDeAcessso;
+    private Map<String, Key> chavesDeAcessso;
 
     public TokenCodificadorDecodificador() {
-        this.chavesDeAcessso = new HashSet<Key>();
+        this.chavesDeAcessso = new HashMap<>();
     }
 
-    public String criarToken(String idUsuario) {
+    public String criarToken(Long idUsuario) {
 
+        String idUsr = String.valueOf(idUsuario);
         Key key = MacProvider.generateKey(SignatureAlgorithm.HS512);
-        this.chavesDeAcessso.add(key);
 
-        String token = Jwts.builder().setId(idUsuario)
+        String token = Jwts.builder().setId(idUsr)
                 .signWith(SignatureAlgorithm.HS512, key)
                 .compact();
+
+        this.chavesDeAcessso.put(token, key);
 
         return token;
     }
 
     public boolean tokenValido(String token) {
-
-        try {
-
-            Jwts.parser().setSigningKey("default").parseClaimsJws(token);
-
-        } catch (SignatureException e) {
-        } catch (MalformedJwtException e) {
-            return false;
-        }
-
-        return true;
+        return this.chavesDeAcessso.containsKey(token);
     }
 
     /**
      * Mudar Exceção para uma mais específica
      */
-    public int decodificarToken(String token) throws Exception {
+    public long decodificarToken(String token) throws Exception {
 
-        if (! tokenValido(token)) {
-            throw new Exception("Token inválido!");
-        }
-
-        Iterator<Key> chavesDeAcesso = this.chavesDeAcessso.iterator();
         Claims claims = null;
 
-        while (chavesDeAcesso.hasNext()) {
-
-            Key chave = chavesDeAcesso.next();
-
-            try {
-                claims = Jwts.parser().setSigningKey(chave).parseClaimsJws(token).getBody();
-            } catch (SignatureException e) {
-            }
+        if (tokenValido(token)) {
+            Key chave = this.chavesDeAcessso.get(token);
+            claims = Jwts.parser().setSigningKey(chave).parseClaimsJws(token).getBody();
         }
 
         if (claims != null) {
-            int id = Integer.parseInt(claims.getId());
+            long id = Long.parseLong(claims.getId());
             return id;
 
         } else
             throw new Exception("Token inválido!");
+    }
+
+    public void removerToken(String token) {
+
+        if (tokenValido(token)) {
+            this.chavesDeAcessso.remove(token);
+        }
+
     }
 }

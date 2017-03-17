1bdae3df75c83a342b61e5db019841a5e4179a57
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-03-17 10:11:21 -0300

    Colocando anotação correta no Usuário repository

--- src/main/java/br/edu/ufcg/computacao/si1/repository/UsuarioRepository.java
+++ src/main/java/br/edu/ufcg/computacao/si1/repository/UsuarioRepository.java
@@ -3,7 +3,9 @@ package br.edu.ufcg.computacao.si1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ufcg.computacao.si1.model.usuario.Usuario;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
 
     Usuario findByEmail(String email);

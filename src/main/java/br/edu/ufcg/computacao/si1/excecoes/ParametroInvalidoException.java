76abeacbd8bf1a7a9723d303044d04d44d8d7c52
davilaerte <davi.nascimento@ccc.ufcg.edu.br>
2017-03-19 20:46:06 -0300

    criando execoes para o sistema

---
+++ src/main/java/br/edu/ufcg/computacao/si1/excecoes/ParametroInvalidoException.java
@@ -0,0 +1,8 @@
+package br.edu.ufcg.computacao.si1.excecoes;
+
+public class ParametroInvalidoException extends AdExtremeException {
+	
+	public ParametroInvalidoException() {
+		super("Atributo invalido!");
+	}
+}
--- src/main/java/br/edu/ufcg/computacao/si1/excecoes/TokenInvalidoException.java
+++ src/main/java/br/edu/ufcg/computacao/si1/excecoes/TokenInvalidoException.java
@@ -3,7 +3,7 @@ package br.edu.ufcg.computacao.si1.excecoes;
 /**
  * Created by luiz on 15/03/17.
  */
-public class TokenInvalidoException extends adExtremeException {
+public class TokenInvalidoException extends AdExtremeException {
 
     public TokenInvalidoException() {
         super("Token inválido!");

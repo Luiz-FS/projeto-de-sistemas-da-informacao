680ae1e1d14af902e1182e7696ce7bdb72c97d7d
antonioPedroNeto <antonio.pedro.neto@ccc.ufcg.edu.br>
2017-03-21 22:53:46 -0300

    src/main/java/br/edu/ufcg/computacao/si1/model/usuario/TiposPermissao.java
    retirando metodos nao utilizados no enum

--- src/main/java/br/edu/ufcg/computacao/si1/model/usuario/TiposPermissao.java
+++ src/main/java/br/edu/ufcg/computacao/si1/model/usuario/TiposPermissao.java
@@ -2,16 +2,7 @@ package br.edu.ufcg.computacao.si1.model.usuario;
 
 public enum TiposPermissao {
 
-    PERMISSAO_PESSOA_FISICA("USER"),
-    PERMISSAO_PESSOA_JURIDICA("COMPANY");
+    PERMISSAO_PESSOA_FISICA,
+    PERMISSAO_PESSOA_JURIDICA;
 
-    private String permissao;
-
-    private TiposPermissao(String permissao) {
-        this.permissao = permissao;
-    }
-
-    public String getPermissao() {
-        return this.permissao;
-    }
 }

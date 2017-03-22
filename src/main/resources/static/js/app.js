fdbf57c72c3125b7c369b8ed6809a26d7b6d1777
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-03-22 13:23:38 -0300

    Iniciando interface de autenticação

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,13 +7,17 @@ var app = angular.module("menuPrincipal", ["ngRoute"]);
 app.config(function($routeProvider, $locationProvider) {
 
 	$routeProvider
-	.when("/cadastrar-se", {
-		templateUrl : "/view/cadastro.html",
-		controller: "controllerCadastro"	
-	})
-	.when("/sobre", {
-	    templateUrl : "/view/sobre.html"
-	});
+		.when("/cadastrar-se", {
+			templateUrl : "/view/cadastro.html",
+			controller: "controllerCadastro"
+		})
+		.when("/login", {
+			templateUrl: "/view/login.html",
+			controller: ""
+		})
+		.when("/sobre", {
+	    	templateUrl : "/view/sobre.html"
+		});
 
 	$locationProvider.html5Mode(true);
 });
---
+++ src/main/resources/static/js/controller/controllerLogin.js
@@ -0,0 +1,7 @@
+/**
+ * Created by luiz on 22/03/17.
+ */
+
+app.controller("controllerLogin", function($scope, $http, $location, $routParams) {
+
+})

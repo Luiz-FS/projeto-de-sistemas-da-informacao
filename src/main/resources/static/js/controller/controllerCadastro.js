ff5e74c6e2608919c3e97ab33ba60311e077c629
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-03-28 23:36:28 -0300

    Removendo logs

--- src/main/resources/static/js/controller/controllerCadastro.js
+++ src/main/resources/static/js/controller/controllerCadastro.js
@@ -11,12 +11,9 @@ app.controller("controllerCadastro", function($scope, $http, $location, $routePa
 			senha: $scope.senhaUsuario,
 			permissao: $scope.opcaoPessoa
 		}
-		
-		console.log(usuario);
+
 		
 		$http.post(CONFIGURACAO.URL + ROTA.USUARIO + SUB_ROTA.CADASTRO, usuario).success(function (data, status) {
-			console.log(data);
-			console.log(status);
 		});
 
         delete $scope.nomeUsuario;
